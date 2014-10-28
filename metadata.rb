59828e7f854c5bf23196c1bac37749122d630a54
Sean M. Sullivan <sean@barriquesoft.com>
2014-10-28 14:34:23 -0700

    Initial repository import.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'webmin'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures webmin'
+long_description 'Installs/Configures webmin'
+version          '0.1.0'
+
